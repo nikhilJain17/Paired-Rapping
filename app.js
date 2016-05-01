8965883aeefb1195d2bdf304eb3ac00ef082becf
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-05-01 19:41:19 -0400

    update server code

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+var app = require("express")();
+var http = require("http").Server(app);
+// var io = require("socket.io")(http);
+
+var PORT = 8000;
+
+app.get('/', function(req, res) {
+	res.send('whats up');
+
+});
 
 
 
 console.log('hello');
+
+
+http.listen(PORT, function() {
+	console.log('Listening on ' + PORT);
+});

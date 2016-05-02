9461928686994675566505a4ce584e9d9ea9b718
nikhilJain17 <nikhil.jain0987@gmail.com>
2016-05-01 20:55:03 -0400

    fixed webhooks to get messages

--- app.js
+++ app.js
@@ -4,11 +4,51 @@ var http = require("http").Server(app);
 
 var PORT = 8000;
 
-app.get('/', function(req, res) {
-	res.send('whats up');
+
+// web hook to verify server is running
+app.get('/messages', function(req, res) {
+ if (req.query['hub.verify_token'] === "nekot")
+  {
+    res.send(req.query['hub.challenge']);
+  }
+  else {
+    res.send('Error, wrong validation token');
+  }
+
+// console.log(req);
 
 });
 
+app.post('/', function(req, res) {
+	console.log(req);
+	res.send('ack');
+});
+
+
+app.get('/', function(req, res) {
+	console.log(req);
+});
+
+
+// web hook to access messages
+app.post('/messages', function(req, res) {
+
+	console.log(req);
+
+// var messaging_events = req.body.entry[0].messaging;
+  
+// //   for (i = 0; i < messaging_events.length; i++) {
+//     var event = req.body.entry[0].messaging[i];
+//     var sender = event.sender.id;
+//     if (event.message && event.message.text) {
+//       var text = event.message.text;
+//       console.log(text);
+// //       // Handle a text message from this sender
+//     }
+//   }
+  res.sendStatus(200);
+
+});
 
 
 console.log('hello');
